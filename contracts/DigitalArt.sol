80dbbf7cec63a93bb39c0f12578dc6a682faa16b
cloudnalu <68409311+cloudnalu@users.noreply.github.com>
2020-12-16 17:48:49 -0600

    contract edits

--- contracts/DigitalArt.sol
+++ contracts/DigitalArt.sol
@@ -1,15 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.8.0;
 
+/// @title A simulator for trees
+/// @author Larry A. Gardner
+/// @notice You can use this contract for only the most basic simulation
+/// @dev All function calls are currently implemented without side effects
+
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/payment/PullPayment.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+
+    /// @notice Calculate tree age in years, rounded up, for live trees
+    /// @dev The Alexandr N. Tetearing algorithm could increase precision
+    /// @param rings The number of rings from dendrochronological sample
+    /// @return age in years, rounded up for partial years
+
+/// @notice provide token IDs for when item is purchased. 
+/// @notice provide token ID for item when uploaded.
+/// @notice create mapping with a key value of the item ID. Set mapping as private to not expose the ID. 
+/// @notice if the item exists.
 contract DigitalArt is ERC721, PullPayment, Ownable {
-    uint256 public _tokenIds;
-    uint256 public _artItemIds;
-    mapping(uint256 => ArtItem) private _artItems;
-    bool isActive = true;
+    uint256 public _tokenIds; 
+    uint256 public _artItemIds; 
+    mapping(uint256 => ArtItem) private _artItems; 
+    bool isActive = true; 
     address admin;
 
     struct ArtItem {
@@ -23,20 +38,28 @@ contract DigitalArt is ERC721, PullPayment, Ownable {
         admin = msg.sender;
     }
 
-    modifier artItemExist(uint256 id) {
+/// @notice check if item exists. If the fuction does not return the passed in item ID, then revert the call. 
+/// @return item ID or error
+    modifier artItemExist(uint256 id) { 
         require(_artItems[id].exists, "Not Found");
         _;
     }
 
+/// @notice seller invokes a public function for adding an item with a price and a tokenURI
+/// @notice check if seller has given a price for the item
+/// @notice create new item object with the item ID value and pass in the parameters
     function addArtItem( uint256 price, string memory tokenURI) 
         public
         contractIsActive
     {
-        require(price > 0, "Price cannot be 0");
+        require(price > 0, "Price cannot be 0"); 
 
-        _artItemIds++;
-        _artItems[_artItemIds] = ArtItem(msg.sender, price, tokenURI, true);
+        _artItemIds++;  /// @notice increment item ID 
+        _artItems[_artItemIds] = ArtItem(msg.sender, price, tokenURI, true); 
     }
+
+/// @notice users invoke a public function to get the items from the mapping with passed in ID. If an item object is found, unwrap it and return the data. 
+/// @return integers and string for the ID
 
     function getArtItem(uint256 id)
         public
@@ -51,7 +74,13 @@ contract DigitalArt is ERC721, PullPayment, Ownable {
         ArtItem memory artItem = _artItems[id];
         return (id, artItem.price, artItem.tokenURI);
     }
-
+/// @notice purchase item, passing in the price and item ID. Make public and payable to enable buyer to send ether to the function. 
+/// @notice get the item object with the passed in item ID and check if exists, then check if the buyer has sent the required amount of ether.
+/// @return the token ID, incremented as a new ID
+/// @notice mint the image and pass the resulting token to the buyer
+/// @notice set the token URI as the IPFS URL of the item
+/// @notice call the pull payment contract to safely transfer and store the incoming payment to an escrow contract
+/// @param for the tranfer is the destination sellers address and the value
     function purchaseArtItem(uint256 artItemId)
         external
         payable
@@ -68,7 +97,7 @@ contract DigitalArt is ERC721, PullPayment, Ownable {
         _setTokenURI(_tokenIds, artItem.tokenURI);
         _asyncTransfer(artItem.seller, msg.value);
     }
-
+/// @notice allow the seller to get and withdraw the payments made in escrow to the contract 
     function getPayments() external {
         withdrawPayments(msg.sender);
     }
@@ -78,6 +107,7 @@ contract DigitalArt is ERC721, PullPayment, Ownable {
     ///       if(msg.sender == owner()) selfdestruct(address(uint160(owner()))); // cast owner to address payable
     ///}
     
+/// @notice allow the admin/owner of the contract to turn the availability of purchasing payments on or off
     function toggleCircuitBreaker() external {
         require(admin == msg.sender);
         isActive = !isActive;
